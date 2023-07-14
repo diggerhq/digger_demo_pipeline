173e2ae8005b07ff3777600e139ad426fb948b48
Igor Zalutski <izalutski@gmail.com>
2023-07-14 16:42:28 +0100

    Update main.tf

--- app_a/main.tf
+++ app_a/main.tf
@@ -1,16 +1,12 @@
-resource "null_resource" "test_dev" {
+resource "null_resource" "test_1" {
 
 }
 
-resource "null_resource" "test_de222222262" {
+resource "null_resource" "test_2" {
 
 }
 
-resource "null_resource" "test_de333" {
-
-}
-
-resource "null_resource" "test_de5555" {
+resource "null_resource" "test_3" {
 
 }
 
